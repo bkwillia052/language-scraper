a6e934594ca8781be920f7ceebfa60d3a334656a
bkwillia052 <bkwillia052@gmail.com>
2019-06-24 00:22:48 -0500

    Updates to paramaters

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
 const request = require("request-promise");
 const cheerio = require("cheerio");
 
+const { JSDOM } = require("jsdom");
+
+const document = new JSDOM(``, {
+  url: "https://tatoeba.org/eng/sentences/show_all_in/spa/eng?page=4",
+  contentType: "text/html"
+}).window.document;
+
 const URL = "https://tatoeba.org/eng/sentences/show_all_in/spa/eng";
-const URL2 = "https://tatoeba.org/eng/sentences/show_all_in/spa/eng?page=4";
+const URL2 = "https://tatoeba.org/eng/sentences/show_all_in/spa/eng?page=2";
 
 (async () => {
   const response = await request(URL2);
 
   let $ = cheerio.load(response);
 
-  let title = $('div[class="sentence-and-translations"]');
-  let rating = $('div[class="text"]').text();
-  let num = $.querySelector(".translations");
+  let totalNum = $(".paging")
+    .find("li:nth-last-child(2) > a")
+    .first();
 
-  console.log(
-    "Title\n",
-    title,
-    "Rating: \n \n",
-    rating,
-    "\n \n \n _____ \n ",
-    num
-  );
+  console.log(`Total Pages: ${totalNum.text()}`);
+  let title = $(".sentence-and-translations").each((index, elem) => {
+    console.log(index);
+    let translation0 = $(elem).find(".text");
+    console.log(translation0.text());
+
+    let translation1 = $(elem)
+      .find(".translation > .text")
+      .prev();
+
+    console.log(translation1.text());
+  });
+
+  //let boxes = document.querySelectorAll(".sentence-and-translations");
+
+  //let translations = Array.from(boxes);
 })();
