3f90547f88a731763e5aeb0f18ed00f5d4d0e62d
bkwillia052 <bkwillia052@gmail.com>
2019-06-30 21:23:13 -0500

    Adding promises

--- index.js
+++ index.js
@@ -9,49 +9,57 @@ const URL2 = "https://tatoeba.org/eng/sentences/show_all_in/spa/eng?page=2";
 let wordKeys = Object.keys(wordDict);
 let cards = [];
 
-let allCards = wordKeys.map(async word => {
-  let URL = `https://tatoeba.org/eng/sentences/search?query=${word}&from=spa&to=eng`;
+let getCards = () => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let word = wordKeys.map(async word => {
+        let URL = `https://tatoeba.org/eng/sentences/search?query=${word}&from=spa&to=eng`;
 
-  try {
-    let cardData = {
-      word: word,
-      sentences: []
-    };
-    let pages = 1;
-    let limit = null;
-    while (limit === null || limit >= pages) {
-      const response = await request(URL);
-
-      let $ = cheerio.load(response);
-
-      let totalNum = $(".paging")
-        .find("li:nth-last-child(2) > a")
-        .first();
-      limit = parseInt(totalNum.text());
-
-      //console.log(`Total Pages: ${limit}`);
-      let title = $(".sentence-and-translations").each((index, elem) => {
-        //console.log(index);
-        let translation0 = $(elem).find(".text");
-        //console.log(translation0.text());
-
-        let translation1 = translation0.text().split("\n");
-        let translationSet = {
-          sp: translation1[1].trim(),
-          en: translation1[2].trim()
+        let cardData = {
+          word: word,
+          sentences: []
         };
-        cardData.sentences.push(translationSet);
+        let pages = 1;
+        let limit = null;
+        while (limit === null || limit >= pages) {
+          const response = await request(URL);
+
+          let $ = cheerio.load(response);
+
+          let totalNum = $(".paging")
+            .find("li:nth-last-child(2) > a")
+            .first();
+          limit = parseInt(totalNum.text());
+
+          //console.log(`Total Pages: ${limit}`);
+          let title = $(".sentence-and-translations").each((index, elem) => {
+            //console.log(index);
+            let translation0 = $(elem).find(".text");
+            //console.log(translation0.text());
+
+            let translation1 = translation0.text().split("\n");
+            let translationSet = {
+              sp: translation1[1].trim(),
+              en: translation1[2].trim()
+            };
+            cardData.sentences.push(translationSet);
+          });
+          pages += 1;
+          //console.log(`Next Page: ${pages}, Limit: ${limit}`);
+          URL = `https://tatoeba.org/eng/sentences/search?query=${word}&from=spa&to=eng&page=${pages}`;
+          //console.log(cardData);
+        }
+        cards.push(cardData);
       });
-      pages += 1;
-      //console.log(`Next Page: ${pages}, Limit: ${limit}`);
-      URL = `https://tatoeba.org/eng/sentences/search?query=${word}&from=spa&to=eng&page=${pages}`;
-      //console.log(cardData);
+      return resolve(cards);
+    } catch (err) {
+      return reject(err);
     }
-    cards.push(cardData);
-    return cardData;
-  } catch (err) {
-    console.log("fuck");
-  }
-});
+  });
+};
+
+getCards()
+  .then(res => console.log("hi"))
+  .catch(err => console.log("FUcK"));
 
 module.exports.handler = () => {};
